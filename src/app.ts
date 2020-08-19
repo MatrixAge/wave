2bea034c450e9942aef445ac1abe78fa42e822eb
isVertical <xiewendao123@foxmail.com>
2020-08-20 02:54:26 +0800

    [fix] fix login error

--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,26 @@ import store from 'store'
 const BASE_URL = 'https://linklink.in'
 
 export const request: RequestConfig = {
-      prefix:BASE_URL,
+	prefix: BASE_URL,
 	timeout: 8000,
 	requestInterceptors: [
 		(url: string) => {
 			if (url === '/login/cellphone') return { url: `${BASE_URL}${url}` }
 
-			const { cookie } = store.get('userinfo')
+                  const userinfo = store.get('userinfo')
 
-			return {
-				url: `${url}?cookie=${cookie}`
+			if (userinfo) {
+				return {
+					url: `${url}?cookie=${userinfo.cookie}`
+				}
+			} else {
+				return {
+					url: `${url}`
+				}
 			}
 		}
 	],
-	errorHandler: async (err: any) => {
+      errorHandler: async (err: any) => {
 		const { code, message, msg } = await err.response.json()
 
 		if (code !== 200) {
